a962a4f680d1e5d41f62b073777efbe4e23bac79
cel01_ <102827591+celocodes@users.noreply.github.com>
2024-08-30 20:05:45 -0300

    aaa

--- hello.py
+++ hello.py
@@ -80,3 +80,22 @@ def index():
     pessoas = User.query.all()  # Obtém todos os registros de usuário do banco de dados
     return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False), pessoas=pessoas, current_time=datetime.utcnow())
+
+@app.route('/professores', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    pessoas = User.query.all()  # Obtém todos os registros de usuário do banco de dados
+    return render_template('professores.html', form=form, name=session.get('name'),
+                           known=session.get('known', False), pessoas=pessoas)
+
