cd5dfc02fcd912684d5b774e66a31eceb4a8efd4
cel01_ <102827591+celocodes@users.noreply.github.com>
2024-08-30 20:20:31 -0300

    aaa

--- hello.py
+++ hello.py
@@ -44,7 +44,8 @@ class User(db.Model):
 
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+    name = StringField('Cadastre o novo Professor:', validators=[DataRequired()])
+    discname = SelectField('Disciplina associada:', choices=[('DSWA5', 'DSWA5'), ('GPSA5', 'GPSA5'), ('IHCA5', 'IHCA5'), ('SODA5', 'SODA5'), ('PJIA5', 'PJIA5'), ('TCOA5', 'TCOA5')])
     submit = SubmitField('Submit')
 
 
